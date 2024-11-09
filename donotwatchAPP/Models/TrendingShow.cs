d947d344549e6af2a49fe6175c09ab567243829d
Ammar <ammarbarakat@std.mans.edu.eg>
2024-11-09 18:48:45 +0200

    Home Page: Add Trending Shows Section

---
+++ donotwatchAPP/Models/TrendingShow.cs
@@ -0,0 +1,13 @@
+﻿namespace donotwatchAPP.Models
+{
+    public class TrendingShow
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string original_name { get; set; }
+        public string overview { get; set; }
+        public string original_language { get; set; }
+        public string first_air_date { get; set; }
+        public string poster_path { get; set; }
+    }
+}
---
+++ donotwatchAPP/Models/TrendingShowsResult.cs
@@ -0,0 +1,9 @@
+﻿namespace donotwatchAPP.Models
+{
+    public class TrendingShowsResult
+    {
+        public int page { get; set; }
+        public int total_results { get; set; }
+        public List<TrendingShow> results { get; set; }
+    }
+}
