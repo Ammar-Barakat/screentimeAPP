185138637ef49085bd9db6db65b7edc5e5751388
Ammar <ammarbarakat@std.mans.edu.eg>
2024-11-24 21:56:56 +0200

    Show Page: Add Info

---
+++ donotwatchAPP/Models/EpisodeDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace donotwatchAPP.Models
+{
+    public class EpisodeDetails
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int season_number { get; set; }
+        public int episode_number { get; set; }
+        public string overview { get; set; }
+        public string air_date { get; set; }
+        public int runtime { get; set; }
+        public string still_path { get; set; }
+    }
+}
---
+++ donotwatchAPP/Models/SeasonDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace donotwatchAPP.Models
+{
+    public class SeasonDetails
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int season_number { get; set; }
+        public string overview { get; set; }
+        public string air_date { get; set; }
+        public List<EpisodeDetails> episodes { get; set; }
+        public string poster_path { get; set; }
+    }
+}
